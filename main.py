f05b8058ef81534da6271ddaa9d122b224347f9d
abichevoy <abichevoy@roku.com>
2022-08-15 11:20:03 -0700

    adding quiz_brain logic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+
 
 question_bank = []
 for question in question_data:
@@ -8,4 +10,7 @@ for question in question_data:
     new_question = Question(q_text=question_text, q_answer=question_answer)
     question_bank.append(new_question)
 
-print(question_bank)
+
+quiz = QuizBrain(q_list=question_bank)
+quiz.next_question()
+
